76d80b44ef6dc02f8e2d636993bfcedeecfae54b
Kotori <543566812@qq.com>
2020-12-23 17:26:24 +0800

    [NeoCPlus] Update Module ColorPrint

--- Toolbox-C++/src/ColorPrint.cpp
+++ Toolbox-C++/src/ColorPrint.cpp
@@ -1,7 +1,98 @@
+/* ColorPrint - v1.0.0.0 */
 #include "ColorPrint.h"
+#include <stdarg.h>
 
-string& ColorPrint::colorStr(string& str, FontFormat fontFormat, FrontColor frontColor, BackColor backColor) {
+/*********************************************************************
+ * 函数名称：colorStr
+ * 函数功能：返回指定颜色和样式的字符串
+ * 输入参数：str        - 目标字符串
+ *           fontFormat - 字符串样式
+ *           frontColor - 字体颜色
+ *           backColor  - 背景颜色
+ * 返回参数：string& 经过装饰后的字符串
+ * 输出结果：未抛出异常，返回经过装饰后的字符串
+ *********************************************************************/
+string ColorPrint::colorStr(const string& str, FontFormat fontFormat, FrontColor frontColor, BackColor backColor) {
 	string format = to_string(fontFormat) + ";" + to_string(frontColor) + ";" + to_string(backColor) + "m";
-	str = "\033[" + format + str + "\033[0m";
-	return str;
+	string styledStr = "\033[" + format + str + "\033[0m";
+	return styledStr;
 }
+
+
+/*********************************************************************
+ * 函数名称：colorPrintf
+ * 函数功能：返回指定颜色和样式的字符串
+ * 输入参数：format     - 格式化输出字符串
+ *           fontFormat - 字符串样式
+ *           frontColor - 字体颜色
+ *           backColor  - 背景颜色
+ * 返回参数：void
+ * 输出结果：打印出有样式和颜色的字符串
+ *********************************************************************/
+void ColorPrint::colorPrintf(const char* format, FontFormat fontFormat, FrontColor frontColor, BackColor backColor, ...) {
+	string str = colorStr(format, RESET, FRONT_RED, BACK_BLACK);
+	const char* colorFormat = str.c_str();
+
+	va_list va;
+	va_start(va, backColor);
+	vprintf(colorFormat, va);
+	va_end(va);
+}
+
+
+/*********************************************************************
+ * 函数名称：colorError
+ * 函数功能：返回指定颜色和样式的字符串
+ * 输入参数：format     - 格式化输出字符串
+ *           ...        - 可变长数组
+ * 返回参数：void
+ * 输出结果：打印出提示错误的字符串
+ *********************************************************************/
+void ColorPrint::colorError(const char* format, ...) {
+	string str = colorStr(format, RESET, FRONT_RED, BACK_BLACK);
+	const char* colorFormat = str.c_str();
+
+	va_list va;
+	va_start(va, format);
+	vprintf(colorFormat, va);
+	va_end(va);
+}
+
+
+/*********************************************************************
+ * 函数名称：colorError
+ * 函数功能：返回指定颜色和样式的字符串
+ * 输入参数：format     - 格式化输出字符串
+ *           ...        - 可变长数组
+ * 返回参数：void
+ * 输出结果：打印出提示正确的字符串
+ *********************************************************************/
+void ColorPrint::colorPass(const char* format, ...) {
+	string str = colorStr(format, RESET, FRONT_GREEN, BACK_BLACK);
+	const char* colorFormat = str.c_str();
+
+	va_list va;
+	va_start(va, format);
+	vprintf(colorFormat, va);
+	va_end(va);
+}
+
+
+/*********************************************************************
+ * 函数名称：colorError
+ * 函数功能：返回指定颜色和样式的字符串
+ * 输入参数：format     - 格式化输出字符串
+ *           ...        - 可变长数组
+ * 返回参数：void
+ * 输出结果：打印出提示警告的字符串
+ *********************************************************************/
+void ColorPrint::colorWarning(const char* format, ...) {
+	string str = colorStr(format, RESET, FRONT_YELLOW, BACK_BLACK);
+	const char* colorFormat = str.c_str();
+
+	va_list va;
+	va_start(va, format);
+	vprintf(colorFormat, va);
+	va_end(va);
+}
+
--- Toolbox-C++/src/include/ColorPrint.h
+++ Toolbox-C++/src/include/ColorPrint.h
@@ -1,53 +1,69 @@
-#ifndef COLOR_PRINT_H
+#ifndef COLOR_PRINT_H  /* ColorPrint - v1.0.0.0 */
 #define COLOR_PRINT_H
 
 #include <string>
+#include <stdarg.h>
 
 using namespace std;
+
+// 通用格式控制:
+enum FontFormat {
+	RESET = 0,      //0 重置所有属性
+	BOLD,           //1 高亮 / 加粗
+	FADE,           //2 暗淡
+	UNDERLINE = 4,  //4 下划线
+	TWINKLE,        //5 闪烁
+	REVERSE,        //7 反转
+	HIDDEN          //8 隐藏
+};
+
+// 字体颜色
+enum FrontColor {
+	FRONT_BLACK = 30, //30 黑色
+	FRONT_RED,        //31 红色
+	FRONT_GREEN,      //32 绿色
+	FRONT_YELLOW,     //33 黄色
+	FRONT_BLUE,       //34 蓝色
+	FRONT_VIOLET,     //35 品红
+	FRONT_CYAN,       //36 青色
+	FRONT_WHITE       //37 白色
+};
+
+// 背景颜色
+enum BackColor {
+	BACK_BLACK = 40, //40 黑色
+	BACK_RED,        //41 红色
+	BACK_GREEN,      //42 绿色
+	BACK_YELLOW,     //43 黄色
+	BACK_BLUE,       //44 蓝色
+	BACK_VIOLET,     //45 品红
+	BACK_CYAN,       //46 青色
+	BACK_WHITE       //47 白色
+};
 
 class ColorPrint
 {
 public:
-	// 通用格式控制:
-	enum FontFormat {
-		RESET = 0,      //0 重置所有属性
-		BOLD,           //1 高亮 / 加粗
-		FADE,           //2 暗淡
-		UNDERLINE = 4,  //4 下划线
-		TWINKLE,        //5 闪烁
-		REVERSE,        //7 反转
-		HIDDEN          //8 隐藏
-	};
 
-	// 字体颜色
-	enum FrontColor {
-		FRONT_BLACK = 30, //30 黑色
-		FRONT_RED,        //31 红色
-		FRONT_GREEN,      //32 绿色
-		FRONT_YELLOW,     //33 黄色
-		FRONT_BLUE,       //34 蓝色
-		FRONT_VIOLET,     //35 品红
-		FRONT_CYAN,       //36 青色
-		FRONT_WHITE       //37 白色
-	};
-
-	// 背景颜色
-	enum BackColor {
-		BACK_BLACK = 40, //40 黑色
-		BACK_RED,        //41 红色
-		BACK_GREEN,      //42 绿色
-		BACK_YELLOW,     //43 黄色
-		BACK_BLUE,       //44 蓝色
-		BACK_VIOLET,     //45 品红
-		BACK_CYAN,       //46 青色
-		BACK_WHITE       //47 白色
-	};
-
-	static string& colorStr(
-		string& str,
-		FontFormat fontFormat = FontFormat::RESET, 
+	static string colorStr(
+		const string& str,
+		FontFormat fontFormat = FontFormat::RESET,
 		FrontColor frontColor = FrontColor::FRONT_WHITE,
 		BackColor backColor = BackColor::BACK_BLACK);
+
+	static void colorPrintf(
+		const char* format,
+		FontFormat fontFormat,
+		FrontColor frontColor,
+		BackColor backColor, ...);
+
+	static void colorError(const char* format, ...);
+
+	static void colorPass(const char* format, ...);
+
+	static void colorWarning(const char* format, ...);
+
+
 };
 
 #endif /* COLOR_PRINT_H */
